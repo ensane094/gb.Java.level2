39c94f26b3dd202d4af692d9b597b7e7441e3b66
Ensane <Ensane094@gmail.com>
2021-10-25 02:46:26 +0300

    v 6.0

--- src/test/java/homework1/Course.java
+++ src/test/java/homework1/Course.java
@@ -4,11 +4,11 @@ public class Course {
 
 
     public void dolt (Barrier []barrier,Team team){
-        for (int i = 0; i <=team.getMembers().length ; i++) {
-            for (int j = 0; j <= barrier.length; j++) {
-
+        for (int i = 0; i <team.getMembers().length ; i++) {
+            for (int j = 0; j < barrier.length; j++) {
+              barrier[j].overcome(team.getMembers()[i]);
             }
-            team.setPassedMembers(i);
+            team.setPassedMembers(i+1);
         }
     }
 }
